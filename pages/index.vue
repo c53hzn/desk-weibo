93a6af467fbf173c1c0b62b79c765d8a4799a9a5
Jenny <hzn2017@outlook.com>
2023-03-02 15:39:05 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ export default {
     var that = this;
     that.$nextTick(() => {
       //add zoompic for imgs
-      zoompic([[".blog-main img",true]]);
+      zoompic([[".desk-unit", true, true, "img"]]);
     });
   },
   head() {
