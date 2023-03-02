2d1d283cc34e2c853ea37525a72cb54b3699a423
Jenny <hzn2017@outlook.com>
2023-03-02 15:37:08 +0800

    Update zoompic.js

--- plugins/zoompic.js
+++ plugins/zoompic.js
@@ -87,6 +87,7 @@ var styleContent = `
   }
 }
 `;
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
 function zoompic(arr) {
 	var d = document;
 	var maskStyle = d.createElement("style");
@@ -102,9 +103,17 @@ function zoompic(arr) {
 		d.querySelector("#mask_layer").className = "fade-out-anime";
 	};
 	for (let j = 0; j < arr.length; j++) {
-		zoom(arr[j][0], arr[j][1], j);
+		let isElementArr = arr[j][2];
+		if (!isElementArr) {
+			zoom1(arr[j][0], arr[j][1], j);
+		} else {
+			let units = d.querySelectorAll(arr[j][0]);
+			for (let m = 0; m < units.length; m++) {
+				zoom2(units[m], arr[j][1], j + alphabet[m], arr[j][3]);
+			}
+		}
 	}
-	function zoom(selector, isImg, groupIndex) {
+	function zoom1(selector, isImg, groupIndex) {
 		var nodes = d.querySelectorAll(selector);
 		if (nodes.length < 2) {
 			d.querySelector(".arrow.arrow-left").style.display = "none";
@@ -122,6 +131,48 @@ function zoompic(arr) {
 					imgObj[groupIndex + "-" + i] = nodes[i].style.backgroundImage;
 				}
 				nodes[i].onclick = function () {
+					d.querySelector(".arrow.arrow-left").setAttribute(
+						"data-zoom-group-index",
+						this.getAttribute("data-zoom-group-index")
+					);
+					d.querySelector(".arrow.arrow-right").setAttribute(
+						"data-zoom-group-index",
+						this.getAttribute("data-zoom-group-index")
+					);
+					if (isImg) {
+						d.querySelector("#mask_child").style.backgroundImage =
+							"url(" + this.src + ")";
+					} else {
+						d.querySelector(
+							"#mask_child"
+						).style.backgroundImage = this.style.backgroundImage;
+					}
+					d.querySelector("#mask_layer").className = "fade-in-anime";
+				};
+			}
+		}
+	}
+	function zoom2(parentNode, isImg, groupIndex, childSelector) {
+		var nodes = parentNode.querySelectorAll(childSelector);
+		for (let i = 0; i < nodes.length; i++) {
+			let parent = nodes[i].parentNode;
+			let grandParent = parent.parentNode;
+			if (parent.nodeName != "A" && grandParent.nodeName != "A") {
+				nodes[i].className = nodes[i].className + " zoomable";
+				nodes[i].setAttribute("data-zoom-group-index", groupIndex + "-" + i);
+				if (isImg) {
+					imgObj[groupIndex + "-" + i] = nodes[i].src;
+				} else {
+					imgObj[groupIndex + "-" + i] = nodes[i].style.backgroundImage;
+				}
+				nodes[i].onclick = function () {
+					if (nodes.length < 2) {
+						d.querySelector(".arrow.arrow-left").style.display = "none";
+						d.querySelector(".arrow.arrow-right").style.display = "none";
+					} else {
+						d.querySelector(".arrow.arrow-left").style.display = "block";
+						d.querySelector(".arrow.arrow-right").style.display = "block";
+					}
 					d.querySelector(".arrow.arrow-left").setAttribute(
 						"data-zoom-group-index",
 						this.getAttribute("data-zoom-group-index")
